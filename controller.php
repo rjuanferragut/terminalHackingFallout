9a10cf43a882cbca7d08a4a82345fc33fef98157
super <lexmengual@gmail.com>
2019-10-10 18:54:17 +0200

    loadWords

--- controller.php
+++ controller.php
@@ -3,6 +3,24 @@
 * @author Alexis Mengual
 */
 
+function loadFile($array_words){
+    $count = 0;
+    $fp = fopen("words.txt", "r");
 
+    while (!feof($fp) AND $count != 6){
+        $linea = fgets($fp);
+        $array_words[$count] = $linea;
+        $count++;
+    }
+
+    fclose($fp);
+    return $array_words;
+}
+
+function printArray($array_words){
+    for ($i = 0; $i < count($array_words); $i++) {
+        echo $array_words[$i];
+    }
+}
 
  ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('controller.php');
+
+$array_words = array();
+
+$array_words = loadFile($array_words);
+printArray($array_words);
+
+ ?>
