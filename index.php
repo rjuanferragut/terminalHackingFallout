f8d6df3bf4672b1dda6a6cab98d267cc6385de52
rafa <rjuanferragut@gmail.com>
2019-10-11 17:21:58 +0200

    tabla con hexadecimal

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
 		// echo $string . "<br />" . strlen($string);
 
 		$string = setWords($array_select_words, $string);
+
+		$array_hexadecimal = array('0x7400','0x74C0','0X740C','0x74CC','0x7418','0x74D0','0x7424','0x74E4','0x7430','0x74F0','0x743C','0x74FC','0x7448','0x7500','0x7454','0x7514','0x7460','0x7520','0x746C','0x752C','0x7478','0x7530','0x7484','0x7544','0x7490','0x7550','0x749C','0x755C','0x74A8','0x7560','0x74B4','0x7574');
+		//array de los codigos hexadecimales ORDENADO POR LINEAS
  	?>
 
  </head>
@@ -34,15 +37,16 @@
  			//creación de las filas y columnas de la tabla
  				$horizontalLines = 16;
  				$verticalLines = 4;
-
- 				for ($i = 0; $i < intval($horizontalLines); $i++){
+ 				$countHexacode = 0;
+ 				for ($i = 0; $i < $horizontalLines; $i++){
  					echo "<tr>";
 
- 						for ($j = 0; $j < int($verticalLines); $j++){
+ 						for ($j = 0; $j < $verticalLines; $j++){
  							if ($j % 2 == 0){
  								//columnas de code hexadecimal
- 								echo "<td class='Hexade'>";
- 								echo "</td>";
+ 								//poner id en los codigos Hexade, con el mismo codigo
+ 								echo "<td class='Hexade'>".$array_hexadecimal[$countHexacode]."</td>";
+ 								$countHexacode++;
 
  							}else{
  								//columnas de code 
