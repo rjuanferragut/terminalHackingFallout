a2948dcdabdd1b7d80d27e435ab80be6f6ccd748
rafa <rjuanferragut@gmail.com>
2019-10-11 18:59:17 +0200

    inici js

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  	 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
  	 <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono&display=swap" rel="stylesheet"> 
  	 <meta name="viewport" content="width=device-width, user-scalable=no">
+ 	  <script src="js/script.js"></script> 
 
  	<?php
 
@@ -27,7 +28,7 @@
 
 
 		$array_hexadecimal = array('0x7400','0x74C0','0X740C','0x74CC','0x7418','0x74D0','0x7424','0x74E4','0x7430','0x74F0','0x743C','0x74FC','0x7448','0x7500','0x7454','0x7514','0x7460','0x7520','0x746C','0x752C','0x7478','0x7530','0x7484','0x7544','0x7490','0x7550','0x749C','0x755C','0x74A8','0x7560','0x74B4','0x7574');
-		//array de los codigos hexadecimales ORDENADO POR LINEAS
+		//array de los codigos hexadecimales ORDENADO POR LINEAS EN HORIZONTAL
 
     // printArray($array_select_words);
     $password = getPassword($array_select_words);
@@ -51,8 +52,11 @@
  			//creación de las filas y columnas de la tabla
  				$horizontalLines = 16;
  				$verticalLines = 4;
+ 				$countLineCode = 1; 
+ 			//empieza en el 1 para que coincida con el total de las strings de codigo
 
  				$countHexacode = 0;
+ 			//contador para seleccionar el índice del array_hexadecicmal
 
 
 
@@ -63,20 +67,20 @@
  							if ($j % 2 == 0){
  								//columnas de code hexadecimal
  								//poner id en los codigos Hexade, con el mismo codigo
- 								echo "<td class='Hexade'>".$array_hexadecimal[$countHexacode]."</td>";
+ 								echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode]."</td>";
  								$countHexacode++;
 
  							}else{
  								//columnas de code
- 								echo "<td class='code'>";
+ 								echo "<td class='code' id=".$countLineCode.">";
+
  								echo "</td>";
+ 								$countLineCode++;
 
  							}
  						}
  					echo "</tr>";
  				}
-
-
 
  			 ?>
  		</table>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+//churro con id = string
+//id = password en value.
+//las td en las que va el codigo van con un id del 1 al 32
+
+//document.getElementById("resultat").innerHTML = "  es negatiu";
+
+	var string = document.getElementById("string").innerHTML;
+	var passwd = document.getElementById("password").innerHTML;
+
+	var countChar = 0;
+
+
+	//for (countChar; countChar < 12; countChar++)
