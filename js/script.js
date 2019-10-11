9aa2f5ed3a3b3189ee42918ded321f2af868a1c6
rafa <rjuanferragut@gmail.com>
2019-10-11 20:00:05 +0200

    intro dades a tabla fail

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 //churro con id = string
 //id = password en value.
 //las td en las que va el codigo van con un id del 1 al 32
+//total 384 char
 
-//document.getElementById("resultat").innerHTML = "  es negatiu";
+	var stringjs = document.getElementById("string").innerHTML;
+	
+	var passwd = document.getElementById("password").value;
+	
 
-	var string = document.getElementById("string").innerHTML;
-	var passwd = document.getElementById("password").innerHTML;
-
+	var countLine = 1;
+	//contador de lineas de codigo, ha de coincidir
 	var countChar = 0;
+	var countLastChar = 11;
+	//contador de caracteres
 
-
-	//for (countChar; countChar < 12; countChar++)
+	for (countLine; countLine <= 32; countLine++){
+		var churro = "";
+		//variable donde se guarda el string de 12 caracteres
+		churro = stringjs.slice(countChar, countLastChar);
+		countChar += 12;
+		countLastChar += 12;
+		
+		document.getElementById(countLine).innerHTML = churro;
+	}
