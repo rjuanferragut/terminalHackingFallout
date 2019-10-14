6802af9d61edcadcfbcf2fe0c821fd0872855e72
rafa <rjuanferragut@gmail.com>
2019-10-14 20:26:04 +0200

    css progress responsive

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
  <html>
  <head>
  	<title>fallout terminal</title>
- 	 <link rel="stylesheet" media="all" type="text/css" href="css/stylesheet.css">
+ 	 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+
  	 <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono&display=swap" rel="stylesheet">
  	 <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0,target-densitydpi=device-dpi, width=device-width, user-scalable=no">
  	  <script src="js/script.js"></script>
@@ -34,62 +35,58 @@
 
  </head>
  <body>
- <div id="container">
- 	<div>
-      <input type="hidden" name="password" id="password" value="<?php echo $password ?>">
-      <span id="string" style="display: none"><?php echo $string?></span>
-  	</div>
-  	<!--
-    <div id="divImage">
-  			<img src="img/foterminal.jpg" alt="TerminalFalloutImage"/>
-  	</div>
-  	-->
+ 	<div id="container">
+	 	<div>
+	      <input type="hidden" name="password" id="password" value="<?php echo $password ?>">
+	      <span id="string" style="display: none"><?php echo $string?></span>
+	  	</div>
 
- 	<div id="divTable">
- 	
- 		<p>Welcome to ROBCO Industries (TM) Termlink ===============================<br />
- 			Password is required <br />
- 		</p>
- 		<p id="lifeCount">	Attempts remaining:</p>
+	 	<div id="divTable">
 
- 		<table>
- 			<?php
- 			//creación de las filas y columnas de la tabla
- 				$horizontalLines = 16;
- 				$verticalLines = 4;
- 				$countLineCode = 1;
- 			//empieza en el 1 para que coincida con el total de las strings de codigo
+	 		<p>Welcome to ROBCO Industries (TM) Termlink ===============================<br />
+	 			Password is required <br />
+	 		</p>
+	 		<p id="lifeCount">	Attempts remaining:</p>
 
- 				$countHexacode = 0;
- 			//contador para seleccionar el índice del array_hexadecicmal
+	 		<table>
+	 			<?php
+	 			//creación de las filas y columnas de la tabla
+	 				$horizontalLines = 16;
+	 				$verticalLines = 4;
+	 				$countLineCode = 1;
+	 			//empieza en el 1 para que coincida con el total de las strings de codigo
+
+	 				$countHexacode = 0;
+	 			//contador para seleccionar el índice del array_hexadecicmal
 
 
 
- 				for ($i = 0; $i < $horizontalLines; $i++){
- 					echo "<tr>";
+	 				for ($i = 0; $i < $horizontalLines; $i++){
+	 					echo "<tr>";
 
- 						for ($j = 0; $j < $verticalLines; $j++){
- 							if ($j % 2 == 0){
- 								//columnas de code hexadecimal
- 								//poner id en los codigos Hexade, con el mismo codigo
- 								echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode]."</td>";
- 								$countHexacode++;
+	 						for ($j = 0; $j < $verticalLines; $j++){
+	 							if ($j % 2 == 0){
+	 								//columnas de code hexadecimal
+	 								//poner id en los codigos Hexade, con el mismo codigo
+	 								echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode]."</td>";
+	 								$countHexacode++;
 
- 							}else{
- 								//columnas de code
- 								echo "<td class='code' id=".$countLineCode." >";
+	 							}else{
+	 								//columnas de code
+	 								echo "<td class='code' id=".$countLineCode." >";
 
- 								echo "</td>";
- 								$countLineCode++;
+	 								echo "</td>";
+	 								$countLineCode++;
 
- 							}
- 						}
- 					echo "</tr>";
- 				}
+	 							}
+	 						}
+	 					echo "</tr>";
+	 				}
 
- 			 ?>
- 		</table>
+	 			 ?>
+	 		</table>
+	 	</div>
+
  	</div>
- </div>
  </body>
  </html>
