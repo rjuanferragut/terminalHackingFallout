7d85326622321cd301950ecb85e50b7acd4f70df
super <lexmengual@gmail.com>
2019-10-14 19:16:44 +0200

    Check if has any alphabet

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@
 //id = password en value.
 //las td en las que va el codigo van con un id del 1 al 32
 //total 384 char
+var positions = [];
+
 document.addEventListener("DOMContentLoaded", function(event){
 	var stringFromPHP = document.getElementById("string").innerText;
 	var passwd = document.getElementById("password").value;
 
-	// var countLine = 1;
-	// //contador de lineas de codigo, ha de coincidir
-	// var countChar = 0;
-	// var countLastChar = 12;
-	// //contador de caracteres
+	var countLine = 1;
+	//contador de lineas de codigo, ha de coincidir
+	var countChar = 0;
+	var countLastChar = 12;
+	//contador de caracteres
 
 	var getFirst = 0;
 	var getLast = 0;
-
-	var positions = [];
 
 	for (var i = 0; i < stringFromPHP.length; i++) {
 			if(stringFromPHP[i].match(/[a-zA-Z ]+/)){
@@ -33,17 +33,29 @@ document.addEventListener("DOMContentLoaded", function(event){
 			}
 	}
 
-	// for (countLine; countLine <= 32; countLine++){
-	// 	var str = "";
-	// 	// Variable donde se guarda el string de 12 caracteres
-	// 	str = stringjs.slice(countChar, countLastChar);
-	//
-	// 	if(str.match(/[a-zA-Z ]+/)){
-	//
-	// 	}
-	//
-	// 	countChar = countLastChar;
-	// 	countLastChar += 12;
-	// 	document.getElementById(countLine).innerHTML = str;
-	// }
+	for (var x = 0; x < positions.length; x++) {
+			if(x % 2 == 0){
+					firstNum = positions[x];
+					lastNum = firstNum + 4;
+			}
+	}
+
+	for (countLine; countLine <= 32; countLine++){
+			var str = "";
+			// Variable donde se guarda el string de 12 caracteres
+			str = stringFromPHP.slice(countChar, countLastChar);
+
+			for (var i = 0; i < str.length; i++) {
+					if(str[i].match(/[a-zA-Z ]+/)){
+							if(getFirst == 0){
+									getFirst = i;
+									str[i] = "¿" + str[i]
+							}
+					}
+			}
+
+			countChar = countLastChar;
+			countLastChar += 12;
+			document.getElementById(countLine).innerHTML = str;
+	}
 });
