d3fe0ca4eb96e322f21f37b9ae331548346bc087
super <lexmengual@gmail.com>
2019-10-14 20:04:36 +0200

    ¿

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@
 //las td en las que va el codigo van con un id del 1 al 32
 //total 384 char
 var positions = [];
+var stringFromPHP;
+
+var getFirst = 0;
+var getLast = 0;
 
 document.addEventListener("DOMContentLoaded", function(event){
-	var stringFromPHP = document.getElementById("string").innerText;
+	stringFromPHP = document.getElementById("string").innerText;
 	var passwd = document.getElementById("password").value;
 
 	var countLine = 1;
@@ -14,24 +18,9 @@ document.addEventListener("DOMContentLoaded", function(event){
 	var countLastChar = 12;
 	//contador de caracteres
 
-	var getFirst = 0;
-	var getLast = 0;
+	var anotherLine = false;
 
-	for (var i = 0; i < stringFromPHP.length; i++) {
-			if(stringFromPHP[i].match(/[a-zA-Z ]+/)){
-					if(getFirst == 0){
-							getFirst = i;
-					}else{
-							if(!stringFromPHP[i + 1].match(/[a-zA-Z ]+/)){
-									getLast = i;
-									positions.push(getFirst);
-									positions.push(getLast);
-
-									getFirst = 0;
-							}
-					}
-			}
-	}
+	calculatePositions();
 
 	for (var x = 0; x < positions.length; x++) {
 			if(x % 2 == 0){
@@ -40,18 +29,25 @@ document.addEventListener("DOMContentLoaded", function(event){
 			}
 	}
 
+	var getAlphabet = false;
+
 	for (countLine; countLine <= 32; countLine++){
 			var str = "";
 			// Variable donde se guarda el string de 12 caracteres
 			str = stringFromPHP.slice(countChar, countLastChar);
-
+			var posStr = 0;
 			for (var i = 0; i < str.length; i++) {
 					if(str[i].match(/[a-zA-Z ]+/)){
-							if(getFirst == 0){
-									getFirst = i;
-									str[i] = "¿" + str[i]
+							if(!getAlphabet){
+								posStr = i;
+								getAlphabet = true;
 							}
 					}
+			}
+
+			if(getAlphabet){
+				str = str.substring(0, posStr) + "¿" + str.substring(posStr, str.length);
+				getAlphabet = false;
 			}
 
 			countChar = countLastChar;
@@ -59,3 +55,21 @@ document.addEventListener("DOMContentLoaded", function(event){
 			document.getElementById(countLine).innerHTML = str;
 	}
 });
+
+function calculatePositions(){
+		for (var i = 0; i < stringFromPHP.length; i++) {
+				if(stringFromPHP[i].match(/[a-zA-Z ]+/)){
+						if(getFirst == 0){
+								getFirst = i;
+						}else{
+								if(!stringFromPHP[i + 1].match(/[a-zA-Z ]+/)){
+										getLast = i;
+										positions.push(getFirst);
+										positions.push(getLast);
+
+										getFirst = 0;
+								}
+						}
+				}
+		}
+}
