cf69c84c4050bed39b3bf62825b433e285dc4b4e
rafa <rjuanferragut@gmail.com>
2019-10-15 17:05:04 +0200

    responsive

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
  	 <link rel="stylesheet" media="(max-width: 16in) and (min-width: 13in)" type="text/css" href="css/stylesheet2.css">
 
- 	 <link rel="stylesheet" media="(max-width:13in)" type="text/css" href="css/stylesheet2.css">
+ 	 <link rel="stylesheet" media="(max-width:13in)" type="text/css" href="css/stylesheet3.css">
 
  	 
 
@@ -47,75 +47,85 @@
 	      <span id="string" style="display: none"><?php echo $string?></span>
 	  	</div>
 
-	 	<div id="divTable">
+		 	<div id="divTable">
 
-	 		<p>Welcome to ROBCO Industries (TM) Termlink ===============================<br />
-	 			Password is required <br />
-	 		</p>
-	 		<p id="lifeCount">	Attempts remaining:</p>
+		 		<p>Welcome to ROBCO Industries (TM) Termlink 
+		 			<br />===============================<br />
+		 			Password is required <br />
+		 		</p>
+		 		<p id="lifeCount">	Attempts remaining:</p>
 
-        <div class="table">
-            <table>
-                <?php
-                    //creación de las filas y columnas de la tabla
-                    $horizontalLines = 16;
-                    $verticalLines = 2;
-                    $countLineCode = 1;
-                    //empieza en el 1 para que coincida con el total de las strings de codigo
+	        	<div class="table">
+	            <table>
+	                <?php
+	                    //creación de las filas y columnas de la tabla
+	                    $horizontalLines = 16;
+	                    $verticalLines = 2;
+	                    $countLineCode = 1;
+	                    //empieza en el 1 para que coincida con el total de las strings de codigo
 
-                    $countHexacode = 0;
+	                    $countHexacode = 0;
 
-                    for ($i=0; $i < $horizontalLines; $i++) {
-                        echo "<tr>";
+	                    for ($i=0; $i < $horizontalLines; $i++) {
+	                        echo "<tr>";
 
-                            for ($j = 0; $j < $verticalLines; $j++) {
-                                if($j % 2 == 0){
-                                    echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode];
-                                    $countHexacode++;
-                                }else{
-                                    echo "<td class='code' id=".$countLineCode." >";
-                                    $countLineCode++;
-                                }
+	                            for ($j = 0; $j < $verticalLines; $j++) {
+	                                if($j % 2 == 0){
+	                                    echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode];
+	                                    $countHexacode++;
+	                                }else{
+	                                    echo "<td class='code' id=".$countLineCode." >";
+	                                    $countLineCode++;
+	                                }
 
-                                echo "</td>";
-                            }
+	                                echo "</td>";
+	                            }
 
-                        echo "</tr>";
-                    }
-                ?>
-            </table>
-        </div>
+	                        echo "</tr>";
+	                    }
+	                ?>
+	            </table>
+	        </div>
 
-        <div class="table">
-            <table>
-                <?php
-                    //creación de las filas y columnas de la tabla
-                    $horizontalLines = 16;
-                    $verticalLines = 2;
-                    //empieza en el 1 para que coincida con el total de las strings de codigo
+	        <div class="table">
+	            <table>
+	                <?php
+	                    //creación de las filas y columnas de la tabla
+	                    $horizontalLines = 16;
+	                    $verticalLines = 2;
+	                    //empieza en el 1 para que coincida con el total de las strings de codigo
 
-                    for ($i=0; $i < $horizontalLines; $i++) {
-                        echo "<tr>";
+	                    for ($i=0; $i < $horizontalLines; $i++) {
+	                        echo "<tr>";
 
-                            for ($j = 0; $j < $verticalLines; $j++) {
-                                if($j % 2 == 0){
-                                    echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode];
-                                    $countHexacode++;
-                                }else{
-                                    echo "<td class='code' id=".$countLineCode." >";
-                                    $countLineCode++;
-                                }
+	                            for ($j = 0; $j < $verticalLines; $j++) {
+	                                if($j % 2 == 0){
+	                                    echo "<td class='Hexade' id=".$array_hexadecimal[$countHexacode].">".$array_hexadecimal[$countHexacode];
+	                                    $countHexacode++;
+	                                }else{
+	                                    echo "<td class='code' id=".$countLineCode." >";
+	                                    $countLineCode++;
+	                                }
 
-                                echo "</td>";
-                            }
+	                                echo "</td>";
+	                            }
 
-                        echo "</tr>";
-                    }
-                ?>
-            </table>
-        </div>
+	                        echo "</tr>";
+	                    }
+	                ?>
+	            </table>
+	        </div>
 	 	</div>
+	 	<div id="prompt">
+	 		<table id="promptTable">
+	 			<tr></tr>
+	 			<tr></tr>
+	 			<tr></tr>
+	 			<tr></tr>
+	 			<tr></tr>
+	 		</table>
 
+	 	</div>
  	</div>
  </body>
  </html>
