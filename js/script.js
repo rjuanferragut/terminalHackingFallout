93116b7c4c15cfb698012386249d396f2cc835a9
LexFTW <lexmengual@gmail.com>
2019-10-23 23:50:39 +0200

    bug fixed

--- js/script.js
+++ js/script.js
@@ -47,12 +47,7 @@ function getTerminal(){
                     td_string = td_string.substring(0, getFirstPosition[i]) + '<span class="span spanId'+(countSpan)+'" onmouseover="hoverSpanOn(this)" onmouseout="hoverSpanOff(this)" onclick="checkWord(this)">' + td_string.substring(getFirstPosition[i], getLastPosition + 1) + '</span>' + td_string.substring(getLastPosition + 1, getFirstPosition[i + 1]) + '<span class="span spanId'+(countSpan+1)+'" onmouseover="hoverSpanOn(this)" onmouseout="hoverSpanOff(this)" onclick="checkWord(this)">' + td_string.substring(getFirstPosition[1], getFirstPosition[i + 1] + 5) + '</span>' + td_string.substring(getFirstPosition[i + 1] + 5, td_string.length);
 
                     getLastPositionSecondWord = getLastPositionLetter(td_string_copy, getFirstPosition[1]);
-                    /*
-                    console.log('Primera posición: ' + getFirstPosition[1]);
-                    console.log('Última posición: ' + getLastPositionSecondWord);
-                    console.log(countCharacter(getFirstPosition[1], getLastPositionSecondWord));
-                    */
-                    if(countCharacter(getFirstPosition[1], getLastPositionSecondWord) + 1 == 5){
+                    if(countCharacter(getFirstPosition[1], getLastPositionSecondWord) == 5){
                       countSpan += 2;
                     }else{
                       countSpan += 1;
@@ -61,13 +56,11 @@ function getTerminal(){
             }
             i++;
         }else{
-          if(countCharacter(getFirstPosition[i], getLastPosition) != 5){
-            if(getLastPosition == 12){
-              td_string = td_string.substring(0, getFirstPosition[i]) + '<span class="span spanId'+(countSpan)+'" onmouseover="hoverSpanOn(this)" onmouseout="hoverSpanOff(this)" onclick="checkWord(this)">' + td_string.substring(getFirstPosition[i], getLastPosition + 1) + '</span>' + td_string.substring(getLastPosition + 1, td_string.length);
-            }else{
+          if(getLastPosition == 12 && countCharacter(getFirstPosition[i], getLastPosition) != 5){
+            td_string = td_string.substring(0, getFirstPosition[i]) + '<span class="span spanId'+(countSpan)+'" onmouseover="hoverSpanOn(this)" onmouseout="hoverSpanOff(this)" onclick="checkWord(this)">' + td_string.substring(getFirstPosition[i], getLastPosition + 1) + '</span>' + td_string.substring(getLastPosition + 1, td_string.length);
+          }else{
               td_string = td_string.substring(0, getFirstPosition[i]) + '<span class="span spanId'+(countSpan)+'" onmouseover="hoverSpanOn(this)" onmouseout="hoverSpanOff(this)" onclick="checkWord(this)">' + td_string.substring(getFirstPosition[i], getLastPosition + 1) + '</span>' + td_string.substring(getLastPosition + 1, td_string.length);
               countSpan += 1;
-            }
           }
         }
       }
