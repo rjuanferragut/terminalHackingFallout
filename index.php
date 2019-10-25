f8a46277fa6d8be1f3d1f9589b18877f2cf78f47
rafa <rjuanferragut@gmail.com>
2019-10-25 17:42:42 +0200

    cascade

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php require_once('views/header.php') ?>
 <link rel="stylesheet" type="text/css" href="css/app.css">
-      <body>
+<body>
 
-        <div class="menu">
-          <a href= "views/terminal.php">Jugar</a>
-          <a href= "views/ranking.php">Ranking</a>
-        </div>
+  <div class="menu">
+    <div class="cascade">
+    </div>
+    <div class="inner">
+      <a href= "views/terminal.php">Jugar</a>
+      <a href= "views/ranking.php">Ranking</a>
+    </div>
+  </div>
 
-      </body>
-  </html>
+
+</body>
+</html>
