030db77b1083c3c3d5ee51ae7f1468c966f72326
rafa <rjuanferragut@gmail.com>
2019-10-25 19:12:14 +0200

    cascade

--- views/register.php
+++ views/register.php
@@ -1,15 +1,18 @@
 <?php
-    require_once('header.php');
- ?>
+require_once('header.php');
+?>
 
- <div class="container">
-    <img src="../img/register.png" alt="" style="width: 30%; display: block; margin: 10px auto;">
-    <h3 style="text-align: center;">¡Tu registro se ha completado correctamente!</h3>
-    <hr style="padding: 2px; background: #00F501; border: 0; margin: 15px auto;" />
-    <h4 style="text-transform: uppercase; text-align: center">Nombre: <?php echo $_POST['prompt'] ?></h4>
-    <h4 style="text-transform: uppercase; text-align: center">Intentos: <?php echo $_POST['tries'] ?></h4>
-    <h4 style="text-transform: uppercase; text-align: center">Tiempo: <?php echo $_POST['game_time'] ?></h4>
-    <br />
-    <a href="terminal.php">Volver a Jugar</a>
-    <a href="../index.php">Página Principal</a>
- </div>
+<div class="container">
+  <div class="cascade">
+  </div>
+  <img src="../img/register.png" alt="" style="width: 30%; display: block; margin: 10px auto;">
+  <h3 style="text-align: center;">¡Tu registro se ha completado correctamente!</h3>
+  <hr style="padding: 2px; background: #00F501; border: 0; margin: 15px auto;" />
+  <h4 style="text-transform: uppercase; text-align: center">Nombre: <?php echo $_POST['prompt'] ?></h4>
+  <h4 style="text-transform: uppercase; text-align: center">Intentos: <?php echo $_POST['tries'] ?></h4>
+  <h4 style="text-transform: uppercase; text-align: center">Tiempo: <?php echo $_POST['game_time'] ?></h4>
+  <br />
+  <a href="terminal.php">Volver a Jugar</a>
+  <a href="../index.php">Página Principal</a>
+
+</div>
