43a31c026257eaaae2497d423c5139d1521e3be7
LexFTW <lexmengual@gmail.com>
2019-11-05 22:12:25 +0100

    jflkñasdjf

--- views/terminal.php
+++ views/terminal.php
@@ -9,11 +9,11 @@
           if($i + 1 == count($array_helps)){
             echo $array_helps[$i];
           }else{
-            echo $array_helps[$i] . ", ";
+            echo $array_helps[$i] . "; ";
           }
         }
       ?></span>
-      <span id="helpers_available"><?php echo $helps_available ?></span>
+      <span id="helpers_available" style="display: none"><?php echo $helps_available ?></span>
     </div>
     <div class="row">
       <div class="col md-12">
@@ -33,7 +33,6 @@
     <div class="row justify-content-center">
       <div class="col md-3" id="terminal-table">
         <?php
-        echo "<script>console.log($username)</script>";
         $countLine = 0;
         $array_hexadecimal = array('0x7400','0x74C0','0X740C','0x74CC','0x7418','0x74D0','0x7424','0x74E4','0x7430','0x74F0','0x743C','0x74FC','0x7448','0x7500','0x7454','0x7514','0x7460','0x7520','0x746C','0x752C','0x7478','0x7530','0x7484','0x7544','0x7490','0x7550','0x749C','0x755C','0x74A8','0x7560','0x74B4','0x7574');
         for ($x = 0; $x < 2; $x++) {
