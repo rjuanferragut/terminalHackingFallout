404bcac7b0aaecadb6ff366bfe72a6b77109ad71
super <lexmengual@gmail.com>
2019-11-05 19:40:12 +0100

    primer push de noviembre

--- views/layouts/terminal.header.php
+++ views/layouts/terminal.header.php
@@ -29,6 +29,11 @@ if(isset($_POST["difficult"])){
 }
 
 session_start();
+if(isset($_SESSION['prompt'])){
+  $username =  $_SESSION['prompt'];
+}else{
+  $username = '';
+}
 setwordLength($word_length);
 $array_words = array();
 $array_select_words = array();
@@ -38,11 +43,8 @@ $array_words = loadFile($filename);
 $array_select_words = selectRandomWords($array_words, $total_words);
 $array_helps = getHelpsAvailables($helps_available);
 
-var_dump($array_helps);
-
 $string = generateSymbolsString();
 $string = setWords($array_select_words, $string, $total_words);
-$string = setHelps($string, $array_helps);
 
 //array de los codigos hexadecimales ORDENADO POR LINEAS EN HORIZONTAL
 $array_hexadecimal = array('0x7400','0x74C0','0X740C','0x74CC','0x7418','0x74D0','0x7424','0x74E4','0x7430','0x74F0','0x743C','0x74FC','0x7448','0x7500','0x7454','0x7514','0x7460','0x7520','0x746C','0x752C','0x7478','0x7530','0x7484','0x7544','0x7490','0x7550','0x749C','0x755C','0x74A8','0x7560','0x74B4','0x7574');
--- views/terminal.php
+++ views/terminal.php
@@ -4,6 +4,16 @@
       <input type="hidden" name="password" id="password" value="<?php echo $password ?>">
       <span id="word_length" style="display: none"><?php echo $word_length ?></span>
       <span id="string" style="display: none"><?php echo $string?></span>
+      <span id="helpers" style="display: none"><?php
+        for ($i=0; $i < count($array_helps); $i++) {
+          if($i + 1 == count($array_helps)){
+            echo $array_helps[$i];
+          }else{
+            echo $array_helps[$i] . ", ";
+          }
+        }
+      ?></span>
+      <span id="helpers_available"><?php echo $helps_available ?></span>
     </div>
     <div class="row">
       <div class="col md-12">
@@ -23,6 +33,7 @@
     <div class="row justify-content-center">
       <div class="col md-3" id="terminal-table">
         <?php
+        echo "<script>console.log($username)</script>";
         $countLine = 0;
         $array_hexadecimal = array('0x7400','0x74C0','0X740C','0x74CC','0x7418','0x74D0','0x7424','0x74E4','0x7430','0x74F0','0x743C','0x74FC','0x7448','0x7500','0x7454','0x7514','0x7460','0x7520','0x746C','0x752C','0x7478','0x7530','0x7484','0x7544','0x7490','0x7550','0x749C','0x755C','0x74A8','0x7560','0x74B4','0x7574');
         for ($x = 0; $x < 2; $x++) {
@@ -48,7 +59,7 @@
               <input type="hidden" name="game_time" value="0">
               <input type="hidden" name="difficult" value="<?php echo $difficult ?>">
               <input type="hidden" name="difficult_value" value="<?php echo $_POST['difficult'] ?>">
-              <input type="hidden" name="prompt" placeholder="<?php echo $_SESSION['prompt']; ?>" class="input-prompt" autocomplete="off">
+              <input type="hidden" name="prompt" placeholder="<?php echo $username ?>" class="input-prompt" autocomplete="off">
             </form>
           </div>
         </div>
